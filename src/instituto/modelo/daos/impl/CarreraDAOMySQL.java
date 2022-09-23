3b8a2a464806f8bb7ed861e75e95a13dca9dc9a2
Charly Cimino <carlosecimino@gmail.com>
2022-09-23 17:46:17 -0300

    Obtener carreras de MySQL

--- src/instituto/modelo/daos/impl/CarreraDAOMySQL.java
+++ src/instituto/modelo/daos/impl/CarreraDAOMySQL.java
@@ -1,8 +1,14 @@
 
 package instituto.modelo.daos.impl;
 
+import instituto.modelo.Conexion;
 import instituto.modelo.daos.CarreraDAO;
 import instituto.modelo.dtos.CarreraDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -12,10 +18,31 @@ import java.util.Collection;
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
 public class CarreraDAOMySQL implements CarreraDAO {
+    
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/instituto";
+    private static final String USER = "root";
+    private static final String PASS = "root";
 
     @Override
-    public Collection<CarreraDTO> obtenerCarreras() {
-        return null;
+    public Collection<CarreraDTO> obtenerCarreras()  {
+        Collection<CarreraDTO> carreras = new ArrayList<CarreraDTO>();
+        try {
+            Connection con = Conexion.getConexion(DRIVER, URL, USER, PASS);
+            
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM carreras");
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                int cod = rs.getInt(1);
+                String nom = rs.getString(2);
+                CarreraDTO carrera = new CarreraDTO(cod, nom);
+                carreras.add(carrera);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al obtener carreras", ex);
+        }
+        return carreras;
     }
 
     @Override
